0c5749f988cfdae860e44316244af5745fba9998
nataliagamezbarea <144036430+nataliagamezbarea@users.noreply.github.com>
2023-12-19 13:06:49 +0100

    Update 1-Código_qr.py

--- 4-App_empresarial/1-Código_qr.py
+++ 4-App_empresarial/1-Código_qr.py
@@ -20,7 +20,7 @@ def generate_qr_with_url(url):
 # Ruta para generar y mostrar el código QR que redirige a una URL
 @app.route('/')
 def generate_qr():
-    url = 'nataliagamezbarea.github.io/EXAMEN-PYTHON-VISUAL/4-App_empresarial/2-Correo_electronico.html'
+    url = 'nataliagamezbarea.github.io/EXAMEN-PYTHON-VISUAL/4-App_empresarial/2-Formulario.html'
     qr_img = generate_qr_with_url(url)
     img_io = BytesIO()
     qr_img.save(img_io, 'PNG')
